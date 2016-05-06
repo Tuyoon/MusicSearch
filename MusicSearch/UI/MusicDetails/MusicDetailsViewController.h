84d60cf3a2bfbe61a686ff38d66fea119c1a3a53
Arkadiy T <>
2016-05-06 17:36:07 +0300

    Add MusicDetailsViewController

---
+++ MusicSearch/UI/MusicDetails/MusicDetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  MusicDetailsViewController.h
+//  MusicSearch
+//
+//  Created by Mac on 06.05.16.
+//  Copyright © 2016 t. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MusicItem;
+
+@interface MusicDetailsViewController : UIViewController
+
+@property (nonatomic, strong) MusicItem *item;
+
+@end
---
+++ MusicSearch/UI/MusicDetails/MusicDetailsViewController.m
@@ -0,0 +1,49 @@
+//
+//  MusicDetailsViewController.m
+//  MusicSearch
+//
+//  Created by Mac on 06.05.16.
+//  Copyright © 2016 t. All rights reserved.
+//
+
+#import "MusicDetailsViewController.h"
+#import "MusicItem.h"
+
+@interface MusicDetailsViewController ()
+
+@end
+
+@implementation MusicDetailsViewController {
+    IBOutlet UILabel *_collectionLabel;
+    IBOutlet UILabel *_artistNameLabel;
+    IBOutlet UILabel *_releaseDateLabel;
+    IBOutlet UILabel *_primaryGenreNameLabel;
+    IBOutlet UIImageView *_artworkImageView;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self updateUI];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)setItem:(MusicItem *)item {
+    _item = item;
+    [self updateUI];
+}
+
+- (void)updateUI {
+    self.title = self.item.trackName;
+    _collectionLabel.text = self.item.collectionName;
+    _artistNameLabel.text = self.item.artistName;
+    _releaseDateLabel.text = self.item.releaseDate;
+    _primaryGenreNameLabel.text = self.item.primaryGenreName;
+    _artworkImageView.image = self.item.artwork100;
+}
+
+@end
