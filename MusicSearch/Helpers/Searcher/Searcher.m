53ae46ea72b81a6d4cfd012b24b96406bf560d37
Arkadiy T <>
2016-05-06 17:34:18 +0300

    Async saving

--- MusicSearch/Helpers/Searcher/Searcher.m
+++ MusicSearch/Helpers/Searcher/Searcher.m
@@ -46,8 +46,10 @@
         }
     };
     [SearchAPI apiWithObject:@{kAPITermKey : term} withSuccessBlock:^(id result) {
-        [self save:result[@"results"]];
-        searchBlock();
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self save:result[@"results"]];
+            searchBlock();
+        });
     } withFailure:^(NSError *error) {
         searchBlock();
     }];
